01765c99db039a06f42ca87792f12676cc543247
Sreekanth V <sreekanthv1995@gmail.com>
2024-05-31 20:02:32 +0530

    address updates3

--- src/app/core/services/chat.service.ts
+++ src/app/core/services/chat.service.ts
@@ -26,7 +26,7 @@ export class ChatService {
       this.disconnect();
     }
     this.currentRoomId = chatRoomId;
-    const url = '//api.maxigcatering.online/ws';
+    const url = '//api.maxigcatering.online/wss';
     // const url = '//localhost:8080/ws';
     const socket = new SockJS(url);
     this.stompClient = Stomp.over(socket);
